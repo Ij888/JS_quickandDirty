eb380aa5e093d6961f4a95606a028f0470757509
Ij888 <ij.ofili@gmail.com>
2014-07-24 07:36:48 +0100

    JS Drills_functions

---
+++ functions.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html> 
+<html lang = "en">
+ <head>
+ <title>screenDimensions</title>
+ <style>#wide{ margin: 0px auto; margin-top: 25%; display:block; text-align:center;}</style>
+ </head>
+ <body>
+	<script>
+		
+	</script>
+	<div id="wide">
+	<button onclick="wWidth();">Get Screen Width</button>
+	
+	</div>
+ </body>
+</html>
