6b5bf7cf327432519fa0a269f4cdc1dda8de1df3
Ij888 <ij.ofili@gmail.com>
2014-08-11 18:19:27 +0100

    reverted changes from deleterious sessison

---
+++ jscript.js
@@ -0,0 +1,5 @@
+//write your script, open terminal and navigate to the root source of your script. With Node already installed type "node nameofscript.js" and hit enter. Simple as.
+
+var b = 3;
+var result = Math.pow(2, b);
+console.log(result);
