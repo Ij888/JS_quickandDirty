9fd6928d46774139c2a4ca15857d7632379bb980
Ij888 <ij.ofili@gmail.com>
2014-10-15 17:32:28 +0100

    added entry

--- moFunctions.js
+++ moFunctions.js
@@ -1,9 +1,31 @@
 //studies in JS engineering
 
-function someFunction(param1, param2){
+
+<!DOCTYPE html>
+<html class="no-js">
+    <head>
+        <title></title>
+        <link rel="stylesheet" href="css/main.css">
+	<style>
+	 body{padding:200px 0 0 150px; font-size:30px; font-family:courier; font-weight:bold;} 
+	 p{color:#999; border-bottom:3px solid #999; display:inline-block;}
+	</style>
+    </head>
+    <body>
+
+        <p>PushStream k177</p>
+		<div  id="sandbox">
+		</div>
+
+
+    <script>
+		function someFunction(param1, param2){
 		var sum = param2 + param1;
 		return sum;
 }
 
  var add = someFunction(3, 4);
 	document.write(add);
+    </script>
+    </body>
+</html>
