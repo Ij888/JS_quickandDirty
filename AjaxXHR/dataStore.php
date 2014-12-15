882b12d6314fa10f3b53c3121b1f38ca3f2e727f
Ij888 <ij.ofili@gmail.com>
2014-12-15 02:04:25 +0000

    server logic

--- AjaxXHR/dataStore.php
+++ AjaxXHR/dataStore.php
@@ -1,3 +1,15 @@
 <?php
+header("content-type: text/xml");
+echo '<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>';
 
+echo '<response>';
+
+	$food =$_GET['food'];
+	$foodArray = array('bool', 'int', 'float', 'string', 'char');
+	if(in_array($food, $foodArray))
+		echo 'Your data type is '.$food'!';
+	elseif($food=='')
+			echo 'Enter another datatype to evaluate your query, '.$food.'! is not an appropriate datatype';
+
+echo '</response>';
 ?>
