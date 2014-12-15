79ff204dc5f4bdace2f48ba4dddab5e1222a2d56
Ij888 <ij.ofili@gmail.com>
2014-12-15 02:07:11 +0000

    wiring

--- AjaxXHR/dataStore.php
+++ AjaxXHR/dataStore.php
@@ -4,12 +4,12 @@ echo '<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>';
 
 echo '<response>';
 
-	$food =$_GET['food'];
-	$foodArray = array('bool', 'int', 'float', 'string', 'char');
-	if(in_array($food, $foodArray))
-		echo 'Your data type is '.$food'!';
-	elseif($food=='')
-			echo 'Enter another datatype to evaluate your query, '.$food.'! is not an appropriate datatype';
+	$dData =$_GET['food'];
+	$dataArray = array('bool', 'int', 'float', 'string', 'char');
+	if(in_array($dData, $dataArray))
+		echo 'Your data type is '.$dData'!';
+	elseif($dData=='')
+			echo 'Enter another datatype to evaluate your query, '.$dData.'! is not an appropriate datatype';
 
 echo '</response>';
 ?>
