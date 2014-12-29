ad4252f6d0d86ddd05b13fa9eb23d9d2e8bd1e5f
Ij888 <ij.ofili@gmail.com>
2014-12-29 18:01:33 +0000

    added entry

---
+++ funObjProMeth.js
@@ -0,0 +1,17 @@
+//authored by Ij888
+function addr(){
+	return 
+}
+
+var post = 
+[
+Lorem Ipsum type post, 
+Goodbye things, 
+one white small book, 
+55 things to say, 
+a tale of two halves, 
+lay it all down, 
+the revolution is not today
+];
+
+var posts = {};
