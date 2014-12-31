e09f9b05988129a4dc54cdfb421a28889a46bd38
Ij888 <ij.ofili@gmail.com>
2014-12-31 13:40:41 +0000

    added entry

--- dateAndTime.js
+++ dateAndTime.js
@@ -1,5 +1,10 @@
 //dateAndTime.js
-var dDate = new Date();
+//var dDate = new Date();
+var futureDate = new Date();
+var today = new Date();
+futureDate.setFullYear(2015, 0, 1);
+var diff = futureDate.getTime() - today.getTime();
+diff = Math.floor(diff / (1000 * 60 * 60 * 24));
 
-document.getElementById("box2").innerHTML = dDate;
+document.getElementById("box2").innerHTML = diff;
  //why the ***k...?!
