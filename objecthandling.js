97af1966301abf9c57828f09b6521a865dd69e6f
Ij888 <ij.ofili@gmail.com>
2015-01-04 04:08:24 +0000

    added entry

--- objecthandling.js
+++ objecthandling.js
@@ -1,3 +1,33 @@
+// create an object with the object literal
 var person1 = {};
-
+//create and assign a property and a value
 person1.fName = 'Jane';
+person1.lName = 'Grey';
+
+/*
+// Create an object with the object literal syntax:
+var person2 = {
+    fName: "Jane",
+    lName: "Doe"
+};
+ 
+alert( person2.fName + " " + person2.lName );
+ 
+var people = {};
+ 
+// Assign properties using "bracket notation"
+// As mentioned, objects can also have objects as a property value
+people[ "person1" ] = person1;
+people[ "person2" ] = person2;
+ 
+// Access properties using a mix of both bracket and dot notation
+alert( people[ "person1" ].fName );
+alert( people[ "person2" ].fName );
+
+
+#If a property is accessed that has not been defined, it will have the value undefined.
+
+// Properties that have not been created are undefined.
+var person = { name: "John Doe" };
+alert( person.email ); // undefined
+*/
