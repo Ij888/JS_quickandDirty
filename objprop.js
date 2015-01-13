bf475605ef1118be706d6267a82b6ac5897d4dc7
Ijem <ij.ofili@gmail.com>
2015-01-13 00:52:45 +0000

    added entry

--- objprop.js
+++ objprop.js
@@ -1,5 +1,5 @@
 var one = {one1:{one2:{one3:{one4:{one5:762}}}}},
 first3objects = one.one1.one2,
-last2objects =  one3.one4,
+last2objects =  first3objects.one3.one4,
 dDom = document.getElementById("test").innerHTML,
-dDom = first3objects.last2objects.one5;
+dDom = last2objects.one5;
