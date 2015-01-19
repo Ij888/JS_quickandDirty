9c140a373a54e165a9786fef861dd6e76f55db5b
Ij888 <ij.ofili@gmail.com>
2015-01-19 01:17:44 +0000

    addedentry

---
+++ cntrlflo2.js
@@ -0,0 +1,10 @@
+var n = 1000;
+
+var multiplesOfThree        = Math.floor((n - 1) /  3);
+/*var multiplesOfFive         = Math.floor((n - 1) /  5);
+var multiplesOfThreeAndFive = Math.floor((n - 1) / 15);
+
+/*var sum =  3 * multiplesOfThree * (multiplesOfThree + 1) / 2
+        +  5 * multiplesOfFive  * (multiplesOfFive  + 1) / 2
+        - 15 * multiplesOfThreeAndFive * (multiplesOfThreeAndFive + 1) / 2;*/
+console.log(multiplesOfThree);
