1bf294a3bb15af577978fbdc6e0c8f6a05971324
Ij888 <ij.ofili@gmail.com>
2015-02-20 16:53:31 +0000

    added entry

--- callback1.js
+++ callback1.js
@@ -1,4 +1,4 @@
 //callBacks
-
-setTimeout(function() {console.log('firstInQueue');}, 500);
+// note the f() {} bit for later analysis
+setTimeout(function() {console.log('firstInQueue'); }, 500);
 console.log('secondInQueue');
