285f462442fe6782b87597b6c1a309ed8260eaf3
Ijem <ij.ofili@gmail.com>
2015-07-28 19:43:13 +0100

    added file

---
+++ dvalue.js
@@ -0,0 +1,9 @@
+function loopty(){
+    setInterval(function(){ 
+        document.getElementById("prgrs").value += 1;
+    }, 1000);
+        
+      
+ }
+
+//how setInterval works: function to be executed, interval to between which to run fn
