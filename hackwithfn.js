6e877e7187ef6213f3165bf43814c902cf6663b6
Ij888 <ij.ofili@gmail.com>
2015-08-18 19:39:14 +0100

    added files
    an object is more suited to store a set of data that can be used
    programatically instead of trying to use functions as seen in the
    changelog of this commit

--- hackwithfn.js
+++ hackwithfn.js
@@ -1,5 +1,9 @@
-var packd = function () {
-    var num = 23;
-    var oObject = {n1: 5, n2: 10, n3: 15, n4: [2, 4, 6, 8]};
-    var tText = "Andromeda Strain";
+var packd = {
+    num1 : 23,
+    oObject : {n1: 5, n2: 10, n3: 15, n4: [2, 4, 6, 8]},
+    tText : "Andromeda Strain"
 };
+
+
+
+console.log(packd.oObject.n2);
