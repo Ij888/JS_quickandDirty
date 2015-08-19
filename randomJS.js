db0f25c8b52f5719ee4d8203c056aa5e49b1e2da
Ij888 <ij.ofili@gmail.com>
2015-08-19 19:39:50 +0100

    added entry

--- randomJS.js
+++ randomJS.js
@@ -2,3 +2,7 @@
  var items = ["k177", "deeke", "PaulWall", "000", "1.5Le"];
         var rand = Math.floor(Math.random()*items.length);
         document.getElementById("show").innerHTML = items[rand];
+
+function showAlert(){
+    alert("@110011@");
+}
