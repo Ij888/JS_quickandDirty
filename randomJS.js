96aba202be8d1a975ced72811d2f03dc79472c9b
Ij888 <ij.ofili@gmail.com>
2015-08-19 19:36:39 +0100

    modified files

--- randomJS.js
+++ randomJS.js
@@ -1,1 +1,4 @@
 //JS 
+ var items = ["k177", "deeke", "PaulWall", "000", "1.5Le"];
+        var rand = Math.floor(Math.random()*items.length);
+        document.getElementById("show").innerHTML = items[rand];
