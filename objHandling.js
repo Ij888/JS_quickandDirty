370b7f86df5f03b4b56d5d68122a28d61d837d62
Ijem <ij.ofili@gmail.com>
2015-08-25 22:12:01 +0100

    added entry

--- objHandling.js
+++ objHandling.js
@@ -1,11 +1,18 @@
+
 var dataStore = {};
-dataStore.d1 = div1;
-dataStore.d2 = div2;
-dataStore.d3 = div3;
+dataStore.d1 = 1024;
+dataStore.d2 = 0.512;
+dataStore.d3 = 3553;
 
 //return dataStore.d2;
 /*
-a return statement can only be used in a function, rather use console.log to output to a nodeJS
-shell
+a return statement can only be used in a function, rather use 
+console.log to output to a nodeJS shell
 */
 
+/*
+also not that the "value" in key:value pairings in objects must
+be declared if it is not a number
+*/
+
+console.log(dataStore.d2);
