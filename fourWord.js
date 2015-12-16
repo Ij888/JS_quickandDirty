f0d7e17c80962215cb8036884a3afeeda62a5d5f
Ijem <ij.ofili@gmail.com>
2015-12-16 18:30:29 +0000

    added entry

--- fourWord.js
+++ fourWord.js
@@ -3,8 +3,8 @@
  */
 
     var aArray, rnd1, rnd2, rnd3, rnd4, fourWord;
-        //aArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-        aArray = ['®', 'B', 'C', 'D'];
+        aArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+        //aArray = ['®', 'B', 'C', 'D'];
         rnd1 = Math.floor(Math.random()*4);
         rnd2 = Math.floor(Math.random()*3);
         rnd3 = Math.ceil(Math.random()*4);
