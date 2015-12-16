aa195ad2f399262fc3ea42b0f567e5258436d23b
Ijem <ij.ofili@gmail.com>
2015-12-16 17:46:32 +0000

    added entry
    Studies in using JS to shuffle letters to form intelligible four-letter
    words.

---
+++ fourWord.js
@@ -0,0 +1,15 @@
+/**
+ * Created by ijemofili on 16/12/15.
+ */
+
+    var aArray, rnd1, rnd2, rnd3, rnd4, fourWord;
+        aArray = [A, B, C, D, E, F, G, H, I, J ,K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z];
+        rnd1 = Math.floor(Math.random()*4);
+        rnd2 = Math.floor(Math.random()*3);
+        rnd3 = Math.ceil(Math.random()*4);
+        rnd4 = Math.floor(Math.random()*3);
+
+fourWord =
+
+    //document.getElementById('box').innerHTML = word;
+console.log(fourWord);
