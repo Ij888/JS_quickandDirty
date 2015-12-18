14ff677edfd0e00299eef724ed683d1f0daa4c7d
Ij888 <ij.ofili@gmail.com>
2015-12-18 23:48:19 +0000

    added entry

--- fourWord.js
+++ fourWord.js
@@ -3,12 +3,15 @@
  */
 
     var aArray, rnd1, rnd2, rnd3, rnd4, fourWord;
-        aArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+        aArray1 = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'];
+        aArray2 = ['N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+        arrayVowels = ['A', 'E', 'I', 'O', 'U'];
+        //aArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
         //aArray = ['®', 'B', 'C', 'D'];
-        rnd1 = Math.floor(Math.random()*4);
+        rnd1 = Math.floor(Math.random()*5);
         rnd2 = Math.floor(Math.random()*3);
-        rnd3 = Math.ceil(Math.random()*4);
-        rnd4 = Math.floor(Math.random()*3);
+        rnd3 = Math.ceil(Math.random()*2);
+        rnd4 = Math.floor(Math.random()*2);
 
 fourWord = aArray[rnd2] + aArray[rnd3] + aArray[rnd4] + aArray[rnd4];
 
