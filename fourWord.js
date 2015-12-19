867fef562988f804585a82e92f4acc1a59de3ca4
Ijem <ij.ofili@gmail.com>
2015-12-19 01:22:52 +0000

    modified entry

--- fourWord.js
+++ fourWord.js
@@ -8,13 +8,13 @@
         arrayVowels = ['A', 'E', 'I', 'O', 'U'];
         //aArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
         //aArray = ['®', 'B', 'C', 'D'];
-<<<<<<< Updated upstream
+
         rnd1 = Math.floor(Math.random()*5);
         rnd2 = Math.floor(Math.random()*3);
         rnd3 = Math.ceil(Math.random()*2);
         rnd4 = Math.floor(Math.random()*2);
 
-=======
+
         rnd1 = Math.floor(Math.random()*4);
         rnd2 = Math.ceil(Math.random()*rnd1);
         rnd3 = Math.ceil(Math.random()*rnd2);
@@ -22,7 +22,7 @@
         if(rnd1 === rnd2){
             rnd2 = Math.ceil(Math.random()*5);
         }
->>>>>>> Stashed changes
+
 fourWord = aArray[rnd2] + aArray[rnd3] + aArray[rnd4] + aArray[rnd4];
 
     //document.getElementById('box').innerHTML = word;
